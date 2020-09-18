0bedb65a74802d63d3be56c6abc75698a17a3f80
Gabriel Weinstock <weinstock.gabriel@gmail.com>
2020-09-17 21:06:24 -0400

    io_service.run, async reads

--- udp-client.cpp
+++ udp-client.cpp
@@ -7,31 +7,28 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 1
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
 #include <thread>
 #include <chrono>
 
 using boost::asio::ip::udp;
-
+boost::asio::io_service io_service;
 udp::socket *sock = nullptr;
 int seq = 0;
 
 boost::array<char, 128> recv_buf;
-boost::array<char, 128> send_buf = { '0', 0 };
 
 void send_handler(const boost::system::error_code& err, std::size_t bytes_sent) {
-	std::cout << "sent " << bytes_sent << " bytes: \"";
-	std::cout.write(send_buf.data(), bytes_sent);
-	std::cout << "\"\n";
-	char ch = (char)seq;
-	send_buf[0] = ch;
+	//std::cout << "sent " << bytes_sent << " bytes.\"";
 }
 
 void recv_handler(const boost::system::error_code& err, std::size_t bytes_recv) {
-	std::cout << "received " << bytes_recv << " bytes: \"";
-	std::cout.write(recv_buf.data(), bytes_recv);
-	std::cout << "\"\n";
-	sock->async_receive(
-		boost::asio::buffer(recv_buf), &recv_handler);
+	std::cout << "received " << bytes_recv << " bytes: ";
+	std::string s(recv_buf.begin(), recv_buf.end());
+	boost::trim_right(s);
+	//std::cout.write(recv_buf.data(), bytes_recv);
+	std::cout << "\"" << s << "\"\n";
+	sock->async_receive(boost::asio::buffer(recv_buf), &recv_handler);
 }
 
 int main(int argc, char* argv[])
@@ -44,8 +41,6 @@ int main(int argc, char* argv[])
 			return 1;
 		}
 
-		boost::asio::io_service io_service;
-
 		udp::resolver resolver(io_service);
 		udp::resolver::query query(udp::v4(), argv[1], "41234"); // port 41234
 		udp::endpoint receiver_endpoint = *resolver.resolve(query);
@@ -55,22 +50,23 @@ int main(int argc, char* argv[])
 		sock->connect(receiver_endpoint);
 
 		// prime the pump:
+		boost::array<char, 128> send_buf = { '0', 0 };
+		
 		sock->async_send(boost::asio::buffer(send_buf), &send_handler);
-		
-		seq++;
-		
+
 		sock->async_receive(
 			boost::asio::buffer(recv_buf), &recv_handler);
 
 		while (seq < 10) {
-			std::cout << "seq: " << seq << std::endl;
-
-			std::this_thread::sleep_for(std::chrono::seconds(2));
-
+			char ch = (char)(seq+48);
+			send_buf[0] = ch;
+			printf_s("ch: '%c'\n", ch);
 			sock->async_send(boost::asio::buffer(send_buf), &send_handler);
-
+			//std::this_thread::sleep_for(std::chrono::seconds(1));
 			seq++;
 		}
+
+		io_service.run();
 
 		sock->close();
 	}
